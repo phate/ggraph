e82a29cc19315095561fb5e5189559b11c09ed2a
Ananya Muddukrishna <ananya.muddukrishna@idi.ntnu.no>
2017-05-09 11:12:09 +0200

    Add new test cases

--- tests/unit_tests/test-aggregation.cpp
+++ tests/unit_tests/test-aggregation.cpp
@@ -84,10 +84,102 @@ test2()
 }
 
 static int
+test3()
+{
+	using namespace ggraph;
+
+	graph g;
+	auto grain1 = create_grain(g, g.entry());
+
+	auto fork1 = create_fork(g, grain1);
+	auto grain2 = create_grain(g, fork1);
+
+	auto fork2 = create_fork(g, grain2);
+	auto grain3 = create_grain(g, fork2);
+	auto grain4 = create_grain(g, fork2);
+	auto join2 = create_join(g, {grain3,grain4});
+
+	auto grain6 = create_grain(g, fork1);
+	auto join3 = create_join(g, {join2, grain6});
+
+	join3->add_successor(g.exit());
+
+	view(g, stdout);
+
+	aggregate(g);
+
+	view(g, stdout);
+
+	is_aggregated(g);
+
+	return 0;
+}
+
+static int
+test4()
+{
+	using namespace ggraph;
+
+	graph g;
+	auto grain1 = create_grain(g, g.entry());
+	grain1->add_successor(g.exit());
+
+	view(g, stdout);
+
+	aggregate(g);
+
+	view(g, stdout);
+
+	is_aggregated(g);
+
+	return 0;
+}
+
+static int
+test5()
+{
+	using namespace ggraph;
+
+	graph g;
+	auto grain1 = create_grain(g, g.entry());
+
+	auto fork1 = create_fork(g, grain1);
+	auto grain2 = create_grain(g, fork1);
+
+	auto fork2 = create_fork(g, grain2);
+	auto grain3 = create_grain(g, fork2);
+	auto grain4 = create_grain(g, fork2);
+	auto join2 = create_join(g, {grain3,grain4});
+
+	auto fork3 = create_fork(g, join2);
+	auto grain5 = create_grain(g, fork3);
+	auto grain6 = create_grain(g, fork3);
+	auto grain7 = create_grain(g, fork3);
+	auto join3 = create_join(g, {grain5,grain6,grain7});
+
+	auto join4 = create_join(g, {join3});
+
+	join4->add_successor(g.exit());
+
+	view(g, stdout);
+
+	aggregate(g);
+
+	view(g, stdout);
+
+	is_aggregated(g);
+
+	return 0;
+}
+
+static int
 test()
 {
 	test1();
 	test2();
+	test3();
+	test4();
+	test5();
 
 	return 0;
 }
