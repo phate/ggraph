8be8889b316d4d07f0d6eda38ffd1254d9c8ef9d
Ananya Muddukrishna <ananya.muddukrishna@idi.ntnu.no>
2017-05-09 11:35:09 +0200

    Make test with valid graph

--- tests/unit_tests/test-ggraph.cpp
+++ tests/unit_tests/test-ggraph.cpp
@@ -13,17 +13,15 @@ test()
 
 	graph g;
 	auto grain1 = create_grain(g, g.entry());
-	auto grain2 = create_grain(g, grain1);
 
-	auto fork = create_fork(g, grain2);
+	auto fork = create_fork(g, grain1);
 	auto grain3 = create_grain(g, fork);
 	auto grain4 = create_grain(g, fork);
 	auto join = create_join(g, {grain3, grain4});
 
-	auto grain5 = create_grain(g, join);
-	grain5->add_successor(g.exit());
+	join->add_successor(g.exit());
 
-	assert(g.nnodes() == 9);
+	assert(g.nnodes() == 7);
 
 	assert(g.entry()->npredecessors() == 0);
 	assert(g.entry()->nsuccessors() == 1);
@@ -32,7 +30,7 @@ test()
 	assert(grain1->npredecessors() == 1);
 	assert(grain1->predecessor(0) == g.entry());
 	assert(grain1->nsuccessors() == 1);
-	assert(grain1->successor(0) == grain2);
+	assert(grain1->successor(0) == fork);
 
 	assert(fork->nsuccessors() == 2);
 	assert(join->npredecessors() == 2);
