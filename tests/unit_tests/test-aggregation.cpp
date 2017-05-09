a64984c98ef0a08a0b6235c28fc742b726612d6b
Ananya Muddukrishna <ananya.muddukrishna@idi.ntnu.no>
2017-05-09 11:37:59 +0200

    Remove tests with invalid graph structures

--- tests/unit_tests/test-aggregation.cpp
+++ tests/unit_tests/test-aggregation.cpp
@@ -23,67 +23,6 @@ is_aggregated(ggraph::graph & g)
 }
 
 static int
-test1()
-{
-	using namespace ggraph;
-
-	graph g;
-	auto grain1 = create_grain(g, g.entry());
-	auto grain2 = create_grain(g, grain1);
-
-	auto fork = create_fork(g, grain2);
-	auto grain3 = create_grain(g, fork);
-	auto grain4 = create_grain(g, fork);
-	auto join = create_join(g, {grain3, grain4});
-
-	auto grain5 = create_grain(g, join);
-	grain5->add_successor(g.exit());
-
-	view(g, stdout);
-
-	aggregate(g);
-
-	view(g, stdout);
-
-	is_aggregated(g);
-
-	return 0;
-}
-
-static int
-test2()
-{
-	using namespace ggraph;
-
-	graph g;
-	auto fork1 = create_fork(g, g.entry());
-
-	auto fork2 = create_fork(g, fork1);
-	auto grain1 = create_grain(g, fork2);
-	auto grain2 = create_grain(g, fork2);
-	auto join2 = create_join(g, {grain1, grain2});
-
-	auto fork3 = create_fork(g, fork1);
-	auto grain3 = create_grain(g, fork3);
-	auto grain4 = create_grain(g, fork3);
-	auto join3 = create_join(g, {grain3, grain4});
-
-	auto join1 = create_join(g, {join2, join3});
-	auto grain5 = create_grain(g, join1);
-	grain5->add_successor(g.exit());
-
-	view(g, stdout);
-
-	aggregate(g);
-
-	view(g, stdout);
-
-	is_aggregated(g);
-
-	return 0;
-}
-
-static int
 test3()
 {
 	using namespace ggraph;
@@ -175,8 +114,6 @@ test5()
 static int
 test()
 {
-	test1();
-	test2();
 	test3();
 	test4();
 	test5();
