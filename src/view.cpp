07b622bf61081d2ef88c4fdef6988dbb3e607bf0
Nico Reissmann <nico.reissmann@gmail.com>
2017-07-07 16:29:57 +0200

    ggraph: emit attributes when printing graph to dot

--- src/view.cpp
+++ src/view.cpp
@@ -11,6 +11,39 @@
 namespace ggraph {
 
 static inline std::string
+emit_string_attribute(const ggraph::attribute & attribute)
+{
+	GGRAPH_DEBUG_ASSERT(is_string_attribute(attribute));
+	auto & sa = *static_cast<const string_attribute*>(&attribute);
+	return strfmt(sa.name(), "=", sa.value());
+}
+
+static inline std::string
+emit_attribute(const ggraph::attribute & attribute)
+{
+	static std::unordered_map<
+		std::type_index,
+		std::string(*)(const ggraph::attribute&)
+	> map({
+	  {std::type_index(typeid(string_attribute)), emit_string_attribute}
+	});
+
+	GGRAPH_DEBUG_ASSERT(map.find(std::type_index(typeid(attribute))) != map.end());
+	return map[std::type_index(typeid(attribute))](attribute);
+}
+
+static inline std::string
+emit_attributes(const operation & op)
+{
+	std::string str;
+	for (const auto & attribute : op) {
+		str += emit_attribute(attribute) + "\\n";
+	}
+
+	return str;
+}
+
+static inline std::string
 node_shape(const node * n)
 {
 	static std::unordered_map<std::type_index, std::string> shape_map({
@@ -31,9 +64,9 @@ emit_node(const node * n)
 {
 	return strfmt(
 		(uintptr_t)n,
-		"[label=",
-		n->operation().debug_string(),
-		" shape=",
+		"[label=\"",
+		n->operation().debug_string(), "\\n", emit_attributes(n->operation()),
+		"\" shape=",
 		node_shape(n),
 		"]");
 }
